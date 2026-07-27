a0b7f0909796d329f381caf05e80fd1df23e9df2
Alexhs  Tassos <27059+atassos@noreply.platform.zone01.gr>
2026-07-27 19:03:01 +0000

    Add js/Handle scroll.js

---
+++ js/Handle scroll.js
@@ -0,0 +1,13 @@
+function handleScrollEvent() {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const totalHeight = document.documentElement.scrollHeight;
+
+    
+    if (scrollPosition >= totalHeight - 100) {
+         
+
+		console.log("Reached the end, loading more posts...")
+        
+    }
+	windows.addEventListener('scroll',handleScrollEvent)
+}
